1ee77c2e7c6390d2b6e4a331f90db7feb3404d1f
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-02-16 18:59:22 +0100

    Vec2Array factory, toIndexedSeq, length

--- vectory/src/main/scala/Geometry.scala
+++ vectory/src/main/scala/Geometry.scala
@@ -59,11 +59,26 @@ object Vec2 {
 }
 
 object Vec2Array {
-  @inline def create(n:Int) = new Vec2Array(InterleavedArray.create(n))
+  @inline def create(n: Int) = new Vec2Array(InterleavedArray.create(n))
+  def apply(vecs: Vec2*) = {
+    val n = vecs.length
+    val v2a = create(n)
+    var i = 0
+    while (i < n) {
+      v2a(i) = vecs(i)
+      i += 1
+    }
+    v2a
+  }
+  implicit def toIndexdSeq(v2a: Vec2Array): IndexedSeq[Vec2] = new IndexedSeq[Vec2] {
+    def apply(idx: Int): Vec2 = v2a(idx)
+    def length: Int = v2a.length
+  }
 }
-@inline final class Vec2Array(val interleaved:InterleavedArray[Double]) {
-  @inline def apply(i:Int) = new Vec2(interleaved.x(i), interleaved.y(i))
-  @inline def update(i:Int, newVec:Vec2) = {
+@inline final class Vec2Array(val interleaved: InterleavedArray[Double]) {
+  @inline def length = interleaved.elementCount
+  @inline def apply(i: Int) = new Vec2(interleaved.x(i), interleaved.y(i))
+  @inline def update(i: Int, newVec: Vec2) = {
     interleaved.updatex(i, newVec.x)
     interleaved.updatey(i, newVec.y)
   }
