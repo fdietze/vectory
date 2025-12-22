874a9268299540da09c1cc4d46d1741008efda5f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-12-22 23:39:56 +0100

    Update scalafmt-core to 3.10.2 (#303)
    * Update scalafmt-core to 3.10.2
    
    * Reformat with scalafmt 3.10.2
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.10.2' to .git-blame-ignore-revs

--- vectory/src/main/scala/ConvexHullOfCircles.scala
+++ vectory/src/main/scala/ConvexHullOfCircles.scala
@@ -144,9 +144,9 @@ object ConvexHullOfCircles2 {
     }
 
     def calculate_p(p_prime: Vec2): Option[Vec2] = {
-      val ray                  = Line(q, p_prime)
-      val angle                = ray.vector.angle
-      val arcOpt               = arcs.find(arcLeaf =>
+      val ray    = Line(q, p_prime)
+      val angle  = ray.vector.angle
+      val arcOpt = arcs.find(arcLeaf =>
         arcLeaf.startAngle <= angle && angle < arcLeaf.endAngle,
       ) // endAngle is exclusive //TODO: wrap around, modulo
       val p: Vec2              = arcOpt match {
